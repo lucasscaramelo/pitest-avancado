25f434f489cef4ab30ef78e244c00c62790de0ec
Lucas Scaramelo <lucas.scaramelo@gmail.com>
2021-04-16 12:39:27 -0300

    feat: Estrutura das classes empregado

--- src/main/java/br/com/pitest/Empregado.java
+++ src/main/java/br/com/pitest/Empregado.java
@@ -1,4 +1,49 @@
 package br.com.pitest;
 
 public class Empregado {
+    private String nome;
+    private String cargo;
+    private float salario;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+
+    public Empregado(String nome, String cargo, float salario) {
+        this.nome = nome;
+        this.cargo = cargo;
+        this.salario = salario;
+    }
+
+    public String introducao() {
+        if (nome != "") {
+            return (String.format("Olá! Meu nome é %s e meu cargo é s%.", getNome(), getCargo()));
+        } else {
+            return "Empregado sem nome";
+        }
+    }
+
+    public void darAumento(float porcento) {
+        salario += (salario * porcento / 100);
+    }
 }
--- src/test/java/br/com/pitest/EmpregadoTest.java
+++ src/test/java/br/com/pitest/EmpregadoTest.java
@@ -1,4 +1,23 @@
 package br.com.pitest;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class EmpregadoTest {
+
+    @Test
+    public void comNome() {
+        Empregado empregado = new Empregado("Lucas", "Programador", (float) 10000);
+        String expected = "Olá! Meu nome é Lucas e meu cargo é Programador.";
+        String result = empregado.introducao();
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void semNome() {
+        Empregado empregado = new Empregado("", "Programador", (float) 10000);
+        String expected = "Empregado sem nome";
+        String result = empregado.introducao();
+        Assert.assertEquals(expected, result);
+    }
 }
